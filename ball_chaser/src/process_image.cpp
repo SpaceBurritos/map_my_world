b169cc7aa76a890ec1f3a54283bdbe89944e1e71
SpaceBurritos <59981149+SpaceBurritos@users.noreply.github.com>
2020-03-11 14:03:40 -0600

    Add files via upload

--- ball_chaser/src/process_image.cpp
+++ ball_chaser/src/process_image.cpp
@@ -8,7 +8,7 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    ROS_INFO_STREAM("Moving the robot in lin x: " + std::to_string(lin_x) + " ang z: " + std::to_string(ang_z));
+    //ROS_INFO_STREAM("Moving the robot in lin x: " + std::to_string(lin_x) + " ang z: " + std::to_string(ang_z));
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
@@ -16,7 +16,7 @@ void drive_robot(float lin_x, float ang_z)
     if(!client.call(srv)){
       ROS_ERROR("Failed to call service command_robot");
     }
-    // TODO: Request a service and pass the velocities to it to drive the robot
+ 
 }
 
 // This callback function continuously executes and reads the image data
@@ -30,12 +30,12 @@ void process_image_callback(const sensor_msgs::Image img)
     double middle = 2*img.step/3;
     bool white_pixel_bool = false;
     int pos;
-    for (int i = 0; i < img.height * img.step; i++){
-      if(img.data[i] == white_pixel){
-	pos = i % img.step;
-	white_pixel_bool = true;
-	break;
-      }
+    for (int i = 0; i < img.height * img.step; i += 3){
+	if(img.data[i] == white_pixel && img.data[i+1] == white_pixel && img.data[i+2] == white_pixel){
+	  pos = i % img.step;
+	  white_pixel_bool = true;
+	  break;
+	}
     }
     if (white_pixel_bool){
       	if (pos <= left){
@@ -48,10 +48,6 @@ void process_image_callback(const sensor_msgs::Image img)
     } else {
 	drive_robot(0,0);
     }
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    // Request a stop when there's no white ball seen by the camera
 }
 
 int main(int argc, char** argv)
